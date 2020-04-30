5b53dcde380cd1796eff3709a337a318fd65c2f1
zyl <316011989@qq.com>
2020-04-30 18:07:38 +0800

    ijk自动播放属性不可以使用 增加调用播放代码

--- ymdy_flutter/lib/page/player_portrait.dart
+++ ymdy_flutter/lib/page/player_portrait.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ijkplayer/flutter_ijkplayer.dart';
 import 'package:ymdy/const/video_datasource.dart';
-import 'package:ymdy/i18n/i18n.dart';
 import 'package:ymdy/utils/options_utils.dart';
 
 /// 竖屏播放器
@@ -42,7 +41,6 @@ class _PlayerPotrait extends State<PlayerPotrait> {
               child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Column(
-
               children: <Widget>[
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -73,12 +71,14 @@ class _PlayerPotrait extends State<PlayerPotrait> {
                   alignment: Alignment.topLeft,
                   padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
                   child: Text(
-                      "\t\t\t\t\t\t正午阳光与爱奇艺达成合作,退出基于余耕(本名王兵)小说《如果没有明天》改编的超级网剧《我是余欢水》,由《都挺好》编剧王三毛、王磊父子改编,孙魔龙指导,侯洪亮制片。",
-                      style: TextStyle(
-                        fontSize: 14.0, // 文字大小
-                      )),
+                    "\t\t\t\t\t\t正午阳光与爱奇艺达成合作,退出基于余耕(本名王兵)小说《如果没有明天》改编的超级网剧《我是余欢水》,由《都挺好》编剧王三毛、王磊父子改编,孙魔龙指导,侯洪亮制片。由《都挺好》编剧王三毛、王磊父子改编,孙魔龙指导,侯洪亮制片。由《都挺好》编剧王三毛、王磊父子改编,孙魔龙指导,侯洪亮制片。",
+                    style: TextStyle(
+                      fontSize: 14.0, // 文字大小
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                  ),
                 ),
-
               ],
             ),
           )),
@@ -88,10 +88,8 @@ class _PlayerPotrait extends State<PlayerPotrait> {
   }
 
   void _playInput() async {
-    await mediaController.setDataSource(
-      VideoDataSource.springBootMenuM3u8,
-      autoPlay: true,
-    );
+    await mediaController.setDataSource(VideoDataSource.springBootMenuM3u8);
+    await mediaController.play();
   }
 
   @override
